9800eb919eeae20485b67dc58ec2ec9637a35f1f
Julián <jgchulio@gmail.com>
2022-11-03 00:56:49 -0300

    Increment event

---
+++ bloc_counter/lib/counter/counter_bloc.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class CounterEvent {}
+
+class CounterIncrementPressed extends CounterEvent {}
+
+class CounterDecrementPressed extends CounterEvent {}
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  @override
+  void onTransition(Transition<CounterEvent, int> transition) {
+    super.onTransition(transition);
+    print('Transtion: $transition');
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print('Error $error $stackTrace');
+    super.onError(error, stackTrace);
+  }
+
+  CounterBloc() : super(0) {
+    on<CounterIncrementPressed>((event, emit) {
+      // handle incoming `CounterIncrementPressed` event
+      return emit(state + 1);
+    });
+    on<CounterDecrementPressed>((event, emit) {
+      // handle incoming `CounterIncrementPressed` event
+      return emit(state - 1);
+    });
+  }
+}
